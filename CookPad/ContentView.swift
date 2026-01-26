bf30f23114238c3eac3781ba3d4ed042b310e619
Mariano Arselan <marse@softserveinc.com>
2026-01-26 15:04:03 -0300

    Initial Commit

---
+++ CookPad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookPad
+//
+//  Created by Mariano Arselan on 26-01-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookPad/CookPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookPadApp.swift
+//  CookPad
+//
+//  Created by Mariano Arselan on 26-01-26.
+//
+
+import SwiftUI
+
+@main
+struct CookPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
